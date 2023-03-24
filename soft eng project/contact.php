913ab2da2516364cb1de728bae5d1fffb6736648
SeymourSchlong <36908123+SeymourSchlong@users.noreply.github.com>
2023-03-24 16:38:45 -0400

    .

--- soft eng project/contact.php
+++ soft eng project/contact.php
@@ -1,7 +1,20 @@
 <?php
+    echo "BALLS"
+/*
     $name       = $_POST['name']; 
     $email      = $_POST['email'];
     $message    = $_POST['message'];
 
     // Database Connection
+    $conn = new mysqli('localhost', 'root', '', 'airnest');
+    if ($conn->connect_error) {
+        die('An error occurred: '.$conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("insert into contact(*, name, email, message) values(? , ? , ? , ?)");
+        $stmt->bind_param("isss", 0, $name, $email, $message);
+        $stmt->execute();
+        echo "we did it!!!";
+        $stmt->close();
+        $conn->close();
+    }*/
 ?>
